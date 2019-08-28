41ebb5170c3a7262e072c51243b3b858cdcd0935
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-28 16:03:21 +0800

    pagbutang nalang sa table ang kulang

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,36 @@
-// basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
 
-client.on("connect", function(){
-    console.log("Successfully connected");
+
+$('#connectBtn').on("click", function () {
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  client.on("connect", function () {
+    $('#status').val("Successfully connected!");
+  })
 })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
+$('#disconnectBtn').on("click", function () {
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  client.on("connect", function () {
+    client.end();
+    $('#status').val("Disconnected!");
+  })
 })
 
-client.publish("mqtt/demo", "hello world!")
+$('#publishBtn').on("click", function () {
+  client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+  })
+  client.publish($('#topic').val(), $('#payload').val())
+})
+
+$('#subscribeBtn').on("click",function(){
+  client.subscribe($('#topic').val())
+})
+
+$('#unsubscribeBtn').on("click",function(){
+  client.subscribe($('#topic').val()+'un')
+  $('#subscriber').val("")
+})
+
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
