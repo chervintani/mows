dc51678d6a13fb3c336867b36e6e332ea430c68b
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-02 13:33:50 +0800

    changed design

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,11 +2,13 @@ $('#connectBtn').on("click", function () {
   var client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
   client.on("connect", function () {
     $('#status').val("Successfully connected!").css("color", "black").css("background-color", "#13d641");
+    console.log("connected");
   })
 
   $('#disconnectBtn').on("click", function () {
-      client.end();
-      $('#status').val("Disconnected!").css("color", "black").css("background-color", "#c71906");
+    client.end();
+    $('#status').val("Disconnected!").css("color", "black").css("background-color", "#c71906");
+    console.log("disconnected");
   })
 
   client.on("message", function (topic, payload) {
@@ -17,39 +19,34 @@ $('#connectBtn').on("click", function () {
   })
 
   $('#publishBtn').on("click", function () {
-    client.publish($('#topic').val(), $('#payload').val(), function (err) {
-      if (err) {
-        alert(err)
-      } else {
-        console.log("published")
-      }
-    })
+    if ($('#topic').val().length == 0) {
+      alert("Please enter a topic");
+    } else {
+      client.publish($('#topic').val(), $('#payload').val(), function () {
+      })
+    }
   })
-  
-  $('#subscribeBtn').on("click", function () {
-    // client.subscribe($('#subscriber').val())
-    client.subscribe($('#subscriber').val(), function (err) {
-      if (err) {
-        console.log(err);
-      } else {
+
+  $(document).on("click",'#subscribeBtn', function () {
+    if ($('#subscriber').val().length == 0) {
+      alert("Please subscribe a topic");
+    } else {
+      client.subscribe($('#subscriber').val(), function () {
         console.log("subscribed")
         $('#subscriber').prop('disabled', true);
-      }
-    })
-  
+      })
+      $(this).prop('disabled', true);
+    }
   })
-  
+
   $('#unsubscribeBtn').on("click", function () {
     $('#subscriber').val("")
-    client.subscribe($('#subscriber').val(), function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("subscribed")
-        $('#subscriber').prop('disabled', true);
-      }
+    client.subscribe($('#subscriber').val(), function () {
+      console.log("unsubscribed")
+      $('#subscriber').prop('disabled', false);
     })
     $('#subscriber').prop('disabled', false);
+    $('#subscribeBtn').prop('disabled', false);
   })
-  
+
 })
